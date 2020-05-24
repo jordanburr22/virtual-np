b86a378f2af7619826802670a3d543ff310b55c0
Sofia DiGirolamo <sdigiro@udel.edu>
2020-05-24 14:24:41 -0400

    Added code to display user data

--- client/src/app/pages/user-data-page/user-data-page.component.ts
+++ client/src/app/pages/user-data-page/user-data-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/security/services/auth.service'
+import { UserService } from 'src/app/security/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-data-page',
@@ -8,10 +9,50 @@ import { AuthService } from 'src/app/security/services/auth.service'
 })
 export class UserDataPageComponent implements OnInit {
 
-  constructor(private _authSvc: AuthService) { }
+  constructor(private router: Router, private userService: UserService) { }
 
-  displayFirstName() {
-    this._authSvc.displayFirstName();
+  getFirstName() {
+    return this.userService.getUser().firstName;
+  }
+
+  getLastName() {
+    return this.userService.getUser().lastName;
+  }
+
+  getDOB() {
+    return this.userService.getUser();
+  }
+
+  getGenderID() {
+    return this.userService.getUser().gender;
+  }
+
+  getAddress() {
+    return this.userService.getUser().address;
+  }
+
+  getCity() {
+    return this.userService.getUser().city;
+  }
+
+  getZip() {
+    return this.userService.getUser().zip;
+  }
+
+  getCountry() {
+    return this.userService.getUser().country;
+  }
+
+  getState() {
+    return this.userService.getUser().state;
+  }
+
+  getEmail() {
+    return this.userService.getUser().email;
+  }
+
+  getPhone() {
+    return this.userService.getUser().phone;
   }
 
   ngOnInit() { }
